6fc9e6c568b68cf3e3c9f977272e75932e009f33
신동주 <sindongju@sindongjuui-MacBookPro.local>
2022-12-05 16:58:58 +0900

    feat: 로그인, 회원가입 로직 구현 완료

---
+++ controllers/users-controllers.js
@@ -0,0 +1,83 @@
+const { validationResult } = require("express-validator");
+
+const HttpError = require("../models/http-error");
+const User = require("../models/user");
+
+const signup = async (req, res, next) => {
+  const errors = validationResult(req); // 유입데이터 체크를 통과하면서 생긴 에러메시지를 받을 수 있다.
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    return next(
+      new HttpError("잘못된 입력입니다. 데이터를 확인해주세요.", 422)
+    );
+  }
+  const { nickName, email, password, checkPassword } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    const error = new HttpError(
+      "회원가입에 실패하셨습니다. 다시 시도해주세요.",
+      500
+    );
+    return next(error);
+  }
+
+  if (existingUser) {
+    const error = new HttpError("이미 등록된 사용자입니다.", 422);
+    return next(error);
+  }
+
+  if (password !== checkPassword) {
+    const error = new HttpError(
+      "비밀번호가 같지 않습니다. 다시 입력해주세요.",
+      422
+    );
+    return next(error);
+  }
+
+  const createdUser = new User({
+    nickName,
+    email,
+    password,
+    checkPassword,
+  });
+
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError("회원가입에 실패하셨습니다.", 500);
+    return next(error);
+  }
+
+  res.status(201).json({ user: createdUser.toObject({ getteers: true }) });
+};
+
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    const error = new HttpError(
+      "로그인에 실패했습니다. 다시 시도해주세요.",
+      500
+    );
+    return next(error);
+  }
+
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError(
+      "등록되지 않은 사용자입니다. 다시 시도해주세요.",
+      401
+    );
+    return next(error);
+  }
+
+  res.json({ message: "로그인에 성공했습니다." });
+};
+
+exports.signup = signup;
+exports.login = login;
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const { check } = require("express-validator");
+
+const usersController = require("../controllers/users-controllers");
 
 const router = express.Router();
 
 router.get("/");
 
-router.post("/signup");
+router.post(
+  "/signup",
+  [
+    check("nickName").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(), // Test@test.com => test@test.com
+    check("password").isLength({ min: 8 }),
+    check("checkPassword").isLength({ min: 8 }),
+  ],
+  usersController.signup
+);
 
-router.post("/login");
+router.post("/login", usersController.login);
 
 module.exports = router;
